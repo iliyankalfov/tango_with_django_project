27c21333b1d321645c0da43fd1e512c53aeb63b0
iliyankalfov <kalfov.iliyan@yahoo.com>
2022-01-19 22:39:33 +0000

    Media files configured and about.html refactored

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 def index(request) :
     context_dic = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dic)
 
 def about(request) :
-    return HttpResponse('<a href="/rango/">Index</a>')
+    return render(request, 'rango/about.html')
 
