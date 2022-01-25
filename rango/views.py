05d87dc9283d24c8158ce5582696ba0ddf1d2953
iliyankalfov <kalfov.iliyan@yahoo.com>
2022-01-25 15:08:58 +0000

    Slugify() added to map URLs without spaces

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+from rango.models import Category
 
 def index(request) :
-    context_dic = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Sorts the categories by likes in descending order('-' before likes)
+    # and retrieves only the first five
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dic = {}
+    context_dic['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dic['categories'] = category_list
+
     return render(request, 'rango/index.html', context=context_dic)
 
 def about(request) :
