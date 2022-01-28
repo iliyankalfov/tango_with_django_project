33f3547d0ae190720afffa2820ee244b0ea98b9e
iliyankalfov <kalfov.iliyan@yahoo.com>
2022-01-28 22:49:45 +0000

    Forms and add_category page created

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 from rango.models import Category, Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 def index(request) :
     # Sorts the categories by likes in descending order('-' before likes)
@@ -31,4 +33,17 @@ def show_category(request, category_name_slug):
         context_dic['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dic)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form':form})
 
