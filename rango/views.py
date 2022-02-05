8b2bbe47efea871cfe9cb2d1023d3f9849b28851
iliyankalfov <kalfov.iliyan@yahoo.com>
2022-02-05 23:47:00 +0000

    Login view and template created

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,8 @@ from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
+
 
 def index(request) :
     # Sorts the categories by likes in descending order('-' before likes)
@@ -105,3 +107,22 @@ def register(request):
     return render(request, 'rango/register.html', context = {'user_form':user_form,
                                                              'profile_form':profile_form,
                                                              'registered':registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
