184909efd720250ab1257b3d5cd28d3fb8ab1ace
iliyankalfov <kalfov.iliyan@yahoo.com>
2022-02-06 00:32:21 +0000

    Access restriction

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,8 @@ from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def index(request) :
@@ -37,6 +38,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dic)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -50,6 +52,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -126,3 +129,12 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html') 
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
