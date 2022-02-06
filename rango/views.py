0e894a18ea554c92673a780dbf099aace35f2b3b
iliyankalfov <kalfov.iliyan@yahoo.com>
2022-02-06 15:09:06 +0000

    Server-side cookies (session) configured

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def index(request) :
@@ -20,10 +21,38 @@ def index(request) :
     context_dic['categories'] = category_list
     context_dic['pages'] = top_five_most_viewed_pages
 
-    return render(request, 'rango/index.html', context=context_dic)
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html', context=context_dic)
+    return response
 
 def about(request) :
-    return render(request, 'rango/about.html')
+    context_dic = {}
+
+    visitor_cookie_handler(request)
+    context_dic['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dic)
+
+def get_server_side_cookie(request, cookie, default_val = None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits','1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
 
 def show_category(request, category_name_slug):
     context_dic = {}
@@ -132,7 +161,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return render(request, 'rango/restricted.html') 
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
